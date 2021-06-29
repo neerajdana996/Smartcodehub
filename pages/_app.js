b2332f7f8da78541090639419df5567435b89e3e
Neeraj Pc <neerajdana9@gmail.com>
2021-06-29 18:47:13 +0530

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,21 @@ import '../styles/globals.css'
 import Link from "next/link";
 function MyApp({ Component, pageProps }) {
   return <div>
-    <div className='h-16 sticky top-0 shadow-sm w-full  flex items-center px-8 justify-between flex-row'>
+    <div className='h-16 bg-white sticky top-0 shadow-sm w-full  flex items-center px-8 justify-between flex-row'>
       <h4 className='text-black text-2xl font-Heading '>Smartcodehub</h4>
       <ul className='text-black  space-x-6 items-center hidden sm:flex' >
         <li><Link href='/'>
           <a>Home</a>
         </Link></li>
-        <li>Docs</li>
+        <li><Link href='/docs'>
+          <a>Docs</a>
+        </Link></li>
         <li><a href='https://blog.smartcodehub.com'>Blog</a></li>
         <li className='px-4 py-2 cursor-pointer bg-black rounded text-white'>
           <a href="https://github.com/Smart-code-hub/Smartcodehub-Docs" target='blank'>
-          Get Started
+            Get Started
           </a>
-         </li>
+        </li>
       </ul>
     </div>
     <Component {...pageProps} />
---
+++ pages/docs/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <div className='flex'>
+            <div className='sm:w-1/5 p-8 sticky flex top-0 text-gray-200 bg-gray-800 h-screen'>
+                <ul className='space-y-5'>
+                    <li className=''>Get Started</li>
+                    <li className=''>Local Setup</li>
+                    <li className=''>Packages Flow</li>
+                    <li className=''>Contribute</li>
+
+                </ul>
+            </div>
+            <div className='flex justify-center py-16 w-full text-3xl font-Heading'>Coming Soon</div>
+        </div>
+    )
+}
