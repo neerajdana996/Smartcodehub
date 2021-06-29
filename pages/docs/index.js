f410f9459b71c3a6e605b1485acc3627cae756ab
Neeraj Pc <neerajdana9@gmail.com>
2021-06-29 21:35:10 +0530

    initial

--- pages/docs/index.js
+++ pages/docs/index.js
@@ -12,7 +12,58 @@ export default function index() {
 
                 </ul>
             </div>
-            <div className='flex justify-center py-16 w-full text-3xl font-Heading'>Coming Soon</div>
-        </div>
-    )
+            <div className='flex justify-center py-16 w-full text-3xl font-lato'>
+                <div class="prose prose-sm sm:prose lg:prose-lg xl:prose-lg mx-auto">
+                    <p>First Lets Clone teh backend and frontend Repositories</p>
+                    <pre>
+                        git clone https://github.com/Smart-code-hub/Smartcodehub-api
+                        <br />
+                        git clone https://github.com/Smart-code-hub/Smartcodehub-Webapp
+
+                    </pre>
+
+                    Once cloned we can create docker-compose file  as folows
+
+                    <pre>
+                        {` 
+version: "3"
+services:
+api:
+    container_name: Smartcodehub-api
+    restart: always
+    build: ./Smartcodehub-api/.
+    ports:
+    - "3666:3666"
+    volumes:
+    - .:/app
+    links:
+    - Smartcodehub-db
+    networks:
+    - Smartcodehub
+app:
+    container_name: Smartcodehub-webapp
+    restart: always
+    build: ./Smartcodehub-webapp/.
+    ports:
+    - "3667:80"
+    volumes:
+    - .:/app  
+    networks:
+    - Smartcodehub       
+Smartcodehub-db:
+    container_name: Smartcodehub-db
+    image: mongo
+    ports:
+    - "27018:27017"
+    networks:
+    - Smartcodehub
+networks:
+Smartcodehub:`}
+                    </pre>
+
+                    <p> Just the final command </p>
+                    <pre>docker-compose up</pre>
+                </div>
+            </div>
+        </div>)
 }
